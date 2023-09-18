9b1302fc3e760152aacaed15112678145c23d83a
Daniel Bretoi <daniel@otherware.org>
2023-09-18 19:57:39 +0700

    add some code

--- lib/porkbun.rb
+++ lib/porkbun.rb
@@ -1,8 +1,59 @@
 # frozen_string_literal: true
 
-require_relative "porkbun/version"
+require 'http'
+require_relative 'porkbun/version'
 
 module Porkbun
   class Error < StandardError; end
-  # Your code goes here...
+  SECRET_KEY = nil
+  API_KEY = nil
+
+  def self.porkbun(path, options = {})
+    res = HTTP.post File.join('https://porkbun.com/api/json/v3', path), json: {
+      secretapikey: SECRET_KEY || ENV.fetch('PORKBUN_SECRET_KEY', nil),
+      apikey: API_KEY || ENV.fetch('PORKBUN_API_KEY', nil)
+    }.merge(options)
+
+    res.parse
+  end
+
+  class Domain
+    def self.list_all
+      Porkbun.porkbun('domain/listAll')
+    end
+  end
+
+  def self.ping
+    porkbun 'ping'
+  end
+
+  class DNS
+    attr_accessor :name, :content, :type, :ttl, :prio, :service, :protocol, :port, :weight, :target
+
+    def initialize(options)
+      @name = options[:name]
+      @content = options[:content]
+      @type = options[:type]
+      @ttl = options[:ttl]
+      @prio = options[:prio]
+      @id = options[:id]
+    end
+
+    def self.create(options)
+      record = DNS.new options
+      record.create
+    end
+
+    def edit
+      raise Error, 'need id to edit' unless @id
+    end
+
+    def create(options)
+      res = Porkbun.porkbun 'dns/create', {
+        ttl: @ttl
+      }.merge(options)
+      @id = res['id']
+      res
+    end
+  end
 end
